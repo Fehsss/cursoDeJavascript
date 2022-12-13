0934e3435eefd9adf9102cf88343504b0e259d3f
phelipeSilvestre <phelipe17silvestre@gmail.com>
2022-12-13 20:52:09 -0300

    novos exercicios

---
+++ exercicios-js/objeto/objetosRevisão.js
@@ -0,0 +1,42 @@
+//coleção dinamica de pares chave/valor
+const produto = new Object
+produto.nome = 'Cadeira'
+produto['marca do produto'] = 'Genérica'
+produto.preco = 220
+
+console.log(produto)
+delete produto.preco
+delete produto['marca do produto']
+console.log(produto)
+
+const carro = {
+    modelo: 'A4',
+    valor: 89000,
+    proprietario: {
+        nome: 'Raul',
+        idade: 56,
+        endereco: {
+            logradouro: 'Rua ABC',
+            numero: 123
+        }
+    },
+    condutores: [{
+        nome: 'Junior',
+        idade: 19
+    }, {
+        nome: 'Ana',
+        idade: 25
+    }],
+    calcularValorSeguro: function() {
+        //...
+    }
+}
+
+carro.proprietario.endereco.numero = 1000
+carro['proprietario']['endereco']['logradouro']
+console.log(carro)
+
+//deletar atributos
+delete carro.proprietario.endereco
+delete carro.calcularValorSeguro
+console.log(carro)
